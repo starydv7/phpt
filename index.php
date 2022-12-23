93bf5de73ae726bd4942d0c375dd484a5a9b325d
starydv7 <pawanstar72926@gmail.com>
2022-12-23 23:24:58 +0530

    phpt

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-$firstName = "Pawan";
-echo "Hello" . " " .$firstName;
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>
+            <?php echo "Hello"?>
+        </h1>
+        <p>My first page</P>
+</body>
+</html>
